4ec0c8945569d2fe8011fbf975b44e3b28b9f7e2
Aditya <adisingh1519@gmail.com>
2023-07-03 16:27:07 +0530

    initial commit

---
+++ routes/tourRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const {
+  getTour,
+  getAllTours,
+  createTour,
+  updateTour,
+  deleteTour,
+  aliasTopTours,
+} = require('./../controllers/tourController');
+
+const router = express.Router();
+
+// router.param('id', checkID);
+
+router
+  .route('/top-5-cheap')
+  .get(aliasTopTours, getAllTours);
+
+router.route('/').get(getAllTours).post(createTour);
+router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+module.exports = router;
